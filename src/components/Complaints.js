fa593999fc757ae6a6540a18ab2e59bb7877e5e1
Aryan Chauhan <aryantkd1011@gmail.com>
2021-12-25 18:04:19 +0530

    Cruds working

--- src/components/Complaints.js
+++ src/components/Complaints.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { FaUser, FaBook, FaAddressBook, FaBible, FaEdit, FaBitbucket } from 'react-icons/fa'
 import {
     BrowserRouter as Router,
     Route,
@@ -8,57 +7,36 @@ import {
     Link,
     useParams
 } from "react-router-dom";
-import Details from './complaintComponents/Details'
 import ComplaintCard from './complaintComponents/ComplaintCard';
 function Complaints({ user }) {
-
     const [data, setData] = useState([]);
-
     const getAllComplaints = async () => {
         const response = await axios.get("http://localhost:5000/api/complaints");
         setData(response.data);
+        console.log('here');
     }
     useEffect(() => {
         getAllComplaints();
     }, [])
-
-
-
-
     return (
         <div className='complaintCards' style={{ backgroundColor: ' rgb(247, 240, 240)' }}>
-
+            
             <Link to="/create">
                 <button>
                     Create Complaint
-
                 </button>
             </Link>
             <h1>All Complaints </h1>
-
-        
             <div className='gridContainer'>
-            {data?.map((d,i)=>{
-            return(
-                <div key={i}  className=" gridChild">
-                        <ComplaintCard complaint={d} />
-                </div>
-            )
-        })}
-                
-
-
-
+                {data?.map((d, i) => {
+                    return (
+                        <div key={i} className=" gridChild">
+                            <ComplaintCard complaint={d} />
+                        </div>
+                    )
+                })}
             </div>
-
-
-
-
         </div>
-
     );
 }
-
-
-
 export default Complaints
